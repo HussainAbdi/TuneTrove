f48c9461ed89164f98e242ce9f72ea15ca25fb62
Hussain Abdi <hussainabdi12@gmail.com>
2023-12-28 17:04:05 -0500

    feat: adds cool style to modal

--- src/styles/StyledModal.js
+++ src/styles/StyledModal.js
@@ -1,13 +1,16 @@
 import Modal from 'styled-react-modal'
 
 const StyledModal = Modal.styled`
-  width: 75%;
-  height: 50%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
   background-color: black;
+  border-radius: 30% 0;
+  width: 75%;
+  height: 50%;
+  min-height: 260px;
+  min-width: 240px;
 `;
 
 export default StyledModal;
