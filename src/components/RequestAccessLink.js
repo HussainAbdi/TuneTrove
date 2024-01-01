e427fa1cd54fc64a5e5c31e929199b75ba223c1b
Hussain Abdi <hussainabdi12@gmail.com>
2024-01-01 02:11:24 -0500

    fix: adds email placeholder for mailto string and removes "how I know you"

--- src/components/RequestAccessLink.js
+++ src/components/RequestAccessLink.js
@@ -12,8 +12,10 @@ const RequestAccess = styled.a`
 
 const mailtoString = `mailto:hussain.abdi@uwaterloo.ca?\
 subject=TuneTrove%3A%20request%20early%20access&\
-body=I'm%20%5BYOUR_NAME%5D%20from%20%5BCOMPANY_NAME%2FHOW_I_KNOW_YOU%5D%20and%20\
-I'd%20like%20to%20request%20early%20access%20to%20TuneTrove!`;
+body=I'm%20%5BYOUR_NAME%5D%20from%20%5BCOMPANY_NAME%5D%20and%20\
+I'd%20like%20to%20request%20early%20access%20to%20TuneTrove%20\
+for%20%5BSPOTIFY_ACCOUNT_EMAIL%5D!
+`;
 
 const RequestAccessLink = ({children}) => (
   <RequestAccess href={mailtoString}>{children}</RequestAccess>
